b7d8bebe30d5e5b66589de4b51a4d528fa1abf44
ausafusa <132696537+ausafusa@users.noreply.github.com>
2026-06-06 23:37:30 +0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ const navItems = [
   { href: "/categories", label: "Categories" },
   { href: "/categories/beauty", label: "Beauty" },
   { href: "/categories/gadgets", label: "Gadgets" },
-   { href: "/categories/gadgets", label: "Home & Kitchen" },
+   { href: "/categories/Home-Kitchen", label: "Home & Kitchen" },
   { href: "/affiliate-disclaimer", label: "Disclosure" },
 ];
 
